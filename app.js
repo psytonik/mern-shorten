6c69b84f7faa533fba48fdaad7961f8fb2b6b61c
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-05-28 22:58:04 +0300

    Redesign this project. more security. still need work on statistics graph

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const helmet = require("helmet");
 const compression = require('compression');
 
 const connectionToDb = require('./dbConfig/connection.js');
 
+app.use(helmet());
+
 app.use(compression());
+
 app.use(cors());
+
 app.use(express.json({extended:true}));
 
 app.use('/api/v1/auth',require('./routes/authRoute.js'));
@@ -15,7 +20,7 @@ app.use('/api/v1/link',require('./routes/linksRoute.js'));
 app.use('/',require('./routes/redirectRoute.js'));
 app.get('/',(req,res)=>{
 	res.status(200).json({message:"hello it\'s test an it works"});
-})
+});
 
 connectionToDb()
 	.finally(()=>{console.log('mongoose connected')});
